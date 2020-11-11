8dcdadfa63ed0c2abeebee1d315fef85690f2acc
Jonathan Barnes <jonathan@codeword.net>
2020-11-11 11:28:44 -0800

    Adds routes to app
    Currently only Home and Tic-Tac-Toe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route
+} from 'react-router-dom';
 import './index.css';
 import TicTacToe from './components/TicTacToe';
 
-ReactDOM.render( <TicTacToe />, document.getElementById('root') );
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav className="nav-bar">
+          <header>Games</header>
+          <ul className="nav-links">
+            <li className="nav-item"><Link to="/">Home</Link></li>
+            <li className="nav-item"><Link to="/tic-tac-toe">Tic Tac Toe</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/tic-tac-toe">
+            <TicTacToe />
+          </Route>
+          <Route path="/">
+            <h2 className="call-out">Choose a game to play!</h2>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render( <App />, document.getElementById('root') );
