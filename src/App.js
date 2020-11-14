743a642617fa39c8cee600e0eb945b49cd0e2150
Jonathan Barnes <jonathan@codeword.net>
2020-11-13 19:49:55 -0800

    Extracted App.js
    also some route refactoring

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import {
+  Switch,
+  Link,
+  Route
+} from 'react-router-dom';
+import _ from 'lodash'
+import TicTacToe from './components/TicTacToe';
+
+const GAMES = [
+  {
+    path: "/tic-tac-toe",
+    title: "Tic-Tac-Toe",
+    component: <TicTacToe />,
+  }
+]
+
+let makeRoute = (page) => {
+  let key = page.path;
+  let link = <li key={key} className="nav-item"><Link to={page.path}>{page.title}</Link></li>;
+  let route = <Route key={key} exact={!!page.exact} path={page.path}>{page.component}</Route>;
+  return {link, route};
+};
+
+let gameRoutes = _.map(GAMES, makeRoute);
+
+let routes = [
+  makeRoute({
+    exact: true,
+    path: "/",
+    title: "Home",  
+    component: <ul className='game-list'>{_.map(gameRoutes, 'link')}</ul>,
+  })
+].concat(gameRoutes);
+
+export default function App() {
+  return (
+    <div>
+      <nav className="nav-bar">
+        <header>Games</header>
+        <ul className="nav-links">{_.map(routes, 'link')}</ul>
+      </nav>
+      <Switch>{_.map(routes, 'route')}</Switch>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Link,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
-import TicTacToe from './components/TicTacToe';
+import App from './App';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav className="nav-bar">
-          <header>Games</header>
-          <ul className="nav-links">
-            <li className="nav-item"><Link to="/">Home</Link></li>
-            <li className="nav-item"><Link to="/tic-tac-toe">Tic Tac Toe</Link></li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/tic-tac-toe">
-            <TicTacToe />
-          </Route>
-          <Route path="/">
-            <h2 className="call-out">Choose a game to play!</h2>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  )
-}
-
-ReactDOM.render( <App />, document.getElementById('root') );
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
